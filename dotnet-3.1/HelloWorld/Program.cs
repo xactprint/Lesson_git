dfcfa693411b0f01f8730dfe3b9699b25de43288
xactprint <xactprint@gmail.com>
2025-05-06 21:23:21 +0100

    Ajout du texte "Hello world again"

--- dotnet-3.1/HelloWorld/Program.cs
+++ dotnet-3.1/HelloWorld/Program.cs
@@ -7,8 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again");
 
-            Console.ReadLine();
+            
         }
     }
 }
